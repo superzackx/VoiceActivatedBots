8fc312b53244cc173a6b06c23bf45cdcb5aa1adb
RageCode <58135394+superzackx@users.noreply.github.com>
2021-01-08 09:25:00 +0530

    Update bot.js

--- Source/bot.js
+++ Source/bot.js
@@ -39,7 +39,59 @@ class Bot {
                 const JokeMsg = new SpeechSynthesisUtterance();
                 JokeMsg.text = randJoke;
                 window.speechSynthesis.speak(JokeMsg);
-            }
+            } else if(command.includes("hi") || command.includes("hello")){
+        var HiMessage = new SpeechSynthesisUtterance();
+        HiMessage.text = "Welcome back sir.";
+        window.speechSynthesis.speak(HiMessage);
+    } else if(command.includes("mail")){
+        var MailMSG = new SpeechSynthesisUtterance();
+        MailMSG.text = "Okay, I have opened your mail now.";
+        window.speechSynthesis.speak(MailMSG);
+        window.open("https://gmail.com")
+    } else if(command.includes("google")){
+        var GoogMsg = new SpeechSynthesisUtterance();
+        GoogMsg.text = "Opened Google";
+        window.speechSynthesis.speak(GoogMsg);
+        window.open("https://google.com")
+    } else if(command.includes("github")){
+        var GitMSG = new SpeechSynthesisUtterance();
+        GitMSG.text = "Here's github!";
+        window.speechSynthesis.speak(GitMSG);
+        console.log("Here's github.")
+        window.open("https://github.com")
+    } else if(command.includes("date")){
+        var time = new Date();
+        var mainTime = time.toDateString();
+        var dateSpeak = new SpeechSynthesisUtterance();
+        dateSpeak.text = "Today is " + mainTime 
+        window.speechSynthesis.speak(dateSpeak);
+    } else if(command.includes("time")){
+        var time = new Date();
+        var timeNow = time.toLocaleTimeString();
+        var timeSpeak = new SpeechSynthesisUtterance();
+        timeSpeak.text = "It's " + timeNow   
+        window.speechSynthesis.speak(timeSpeak);
+    } else if(command.includes("search for")){
+        var split = command.split(" ")
+        var seartchSpeak = new SpeechSynthesisUtterance();
+        seartchSpeak.text = "Showing resultst for that on the internet."
+        window.speechSynthesis.speak(seartchSpeak);
+        window.open("https://google.com/search?q=" + split[2])
+    } else if(command.includes("discord")){
+        speak("Here's discord, sir.")
+        window.open("https://www.discord.com/app")
+    } else if(command.includes("npm")){
+        speak("Here's npmjs")
+        window.open("https://npmjs.com")
+    } else if(command.includes("repl.it")){
+        speak("Opening repl.it")
+        window.open("https://repl.it")
+    } else{
+        var fallback = new SpeechSynthesisUtterance();
+        fallback.text = "I don't understand that";
+        window.speechSynthesis.speak(fallback);
+    }
+            
             recognition.onspeechend = function() {
                 recognition.stop();
             }
